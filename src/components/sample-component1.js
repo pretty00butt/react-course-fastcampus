cf5d250555b03b5d3ae126e0f8b4bd19523b4c75
the6thm0nth <hoonch3@gmail.com>
2018-02-03 21:03:24 +0900

    update week-1

---
+++ src/components/sample-component1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SampleComponent1 extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Component 1</h1>
+        <p>이 곳은 컴포넌트 1 입니다.</p>
+      </section>
+    );
+  }
+}
+
+export default SampleComponent1;
